a6caa0b8d5267d9f6220039223ce1687f9b11162
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2024-02-11 13:26:10 +0300

    Add files via upload

---
+++ 35.Foreach Döngüsü/app.js
@@ -0,0 +1,7 @@
+//   FOREACH DÖNGÜSÜ
+
+let isimler = ["Biltekin", "Enes", "Kübra", "Ayşenur", "Adem", "Yakub"];
+
+isimler.forEach(function (isim) {
+  console.log(isim);
+}); // bu döngüyü aslında for ve while döngüsü ile de yapabilirsiniz aynıdır.
